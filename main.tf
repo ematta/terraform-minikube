44233ffee6a76475e8de06dfa501851c2479b0cb
Enrique <enrique.j.matta@gmail.com>
2022-05-21 09:35:34 -0500

    init helm

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
   }
 }
 
@@ -11,8 +15,19 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 resource "kubernetes_namespace" "minikube-namespace" {
   metadata {
     name = "tf-minikube-namespace"
   }
+}
+
+resource "helm_release" "local" {
+  name          = "buildachart"
+  chart         = "./buildachart"
 }
